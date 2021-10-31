16345da74ad934d3828d824a362c096d8ef6b728
Raphael Adams <rydraf@gmail.com>
2021-10-31 19:15:29 +0000

    about page flask

--- pythonlab/main.py
+++ pythonlab/main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-    
-@app.route("/raphael")
-def raphael():
-    return "Hello, Raphael"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
     
 if __name__ == "__main__":
