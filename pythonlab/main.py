f10f4c92b2699b17b7ab5c9f351afe866b0939ed
Raphael Adams <rydraf@gmail.com>
2021-10-31 18:57:50 +0000

    route update /raphael

--- pythonlab/main.py
+++ pythonlab/main.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/raphael")
+def raphael():
+    return "Hello, Raphael"
+
     
 if __name__ == "__main__":
     app.run(debug=True)
